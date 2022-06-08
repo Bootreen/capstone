0a726a1da4378d4d42afd1494f50d3ababfcec5d
Alex Boot <bootreen@gmail.com>
2022-06-08 18:46:08 +0300

    first steps and renaming branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Sauces'
+    },
+    {
+      id: 2,
+      title: 'Ground Chili'
+    },
+    {
+      id: 3,
+      title: 'Fresh Chili'
+    },
+    {
+      id: 4,
+      title: 'Mild'
+    },
+    {
+      id: 5,
+      title: 'Hot'
+    }
+  ];
+
   return (
-    <div>
-      Placeholder
+    <div className='categories-container'>
+      {categories.map(({ id, title }) => 
+        (<div className='category-container'>
+            {/* <img /> */}
+            <div id={id} className='category-body-container'>
+              <h2>{title}</h2>
+              <p>Shop now</p>
+            </div>
+          </div>)
+      )}      
     </div>
   );
 }
