ce94f9ebb4fbe4e3c74e8c8d09b8479126d6df85
Alex Boot <bootreen@gmail.com>
2022-06-10 12:42:15 +0300

    Adding React-router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import CategoriesList from './components/categories-list/categories-list.component.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
 
-const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Sauces',
-      imageUrl: 'https://tangelocat.com/images/sauces-min.jpg'
-    },
-    {
-      id: 2,
-      title: 'Ground Chili',
-      imageUrl: 'https://tangelocat.com/images/ground-chili-min.jpg'
-    },
-    {
-      id: 3,
-      title: 'Fresh Chili',
-      imageUrl: 'https://tangelocat.com/images/fresh-chili-min.jpg'
-    },
-    {
-      id: 4,
-      title: 'Mild',
-      imageUrl: 'https://tangelocat.com/images/mild-min.jpg'
-    },
-    {
-      id: 5,
-      title: 'Hot',
-      imageUrl: 'https://tangelocat.com/images/hot-min.jpg'
-    }
-  ];
-  
-  return (
-    <CategoriesList categories={categories} />    
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path='/' element={<Home />} />    
+  </Routes>  
+);
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,38 @@
+import CategoriesList from '../../components/categories-list/categories-list.component.jsx';
+
+const Home = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Sauces',
+      imageUrl: 'https://tangelocat.com/images/sauces-min.jpg'
+    },
+    {
+      id: 2,
+      title: 'Ground Chili',
+      imageUrl: 'https://tangelocat.com/images/ground-chili-min.jpg'
+    },
+    {
+      id: 3,
+      title: 'Fresh Chili',
+      imageUrl: 'https://tangelocat.com/images/fresh-chili-min.jpg'
+    },
+    {
+      id: 4,
+      title: 'Mild',
+      imageUrl: 'https://tangelocat.com/images/mild-min.jpg'
+    },
+    {
+      id: 5,
+      title: 'Hot',
+      imageUrl: 'https://tangelocat.com/images/hot-min.jpg'
+    }
+  ];
+  
+  return (
+    <CategoriesList categories={categories} />    
+  );
+}
+
+export default Home;
